dc72e0363a840966222c8b65be7c9db44722ea75
TheAkylino <40051292+TheAkylino@users.noreply.github.com>
2021-10-18 01:22:42 -0500

    Initial commit
    INITIAL COMMIT JPA-ONE-TO-ONE-DEMO-01

--- api-demo-v1-app/src/main/java/com/example/models/dao/CustomersDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.models.dao;
-
-import com.example.models.entity.Customers;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomersDao extends JpaRepository<Customers,Long> {
-}
